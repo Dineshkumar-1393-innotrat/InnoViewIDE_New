36da0921625fb810c3a2e030a7997f8be3f89279
Dineshkumar-1393-innotrat <dineshkumar@innotrat.in>
2025-12-18 13:09:08 +0530

    Add Home component featuring user login, authentication, and real-time date/time display.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -345,7 +345,7 @@ const Home = () => {
                     Create an account
                   </ChakraLink>
                 </Flex>
-                <Text textAlign="center" fontSize="xs" color="green.500">InnoIDE_Rev0.7_28-11-2025 (C) Innotrat Labs</Text>
+                <Text textAlign="center" fontSize="xs" color="green.500">InnoIDE_Rev0.8_18-12-2025 (C) Innotrat Labs</Text>
               </VStack>
             </Flex>
           </Box>
